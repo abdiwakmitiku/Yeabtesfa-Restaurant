ac175fc89640e3d2886049e953147beae9258529
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-12 16:33:56 +0300

    final-Result

--- client/src/components/Sections/MenuCard.jsx
+++ client/src/components/Sections/MenuCard.jsx
@@ -6,6 +6,8 @@
 //     .filter((key) => key.startsWith("Property"))
 //     .map((key) => data[key]);
 
+//   let flip = true;
+
 //   return (
 //     <div className={styles.page}>
 //       <header>
@@ -17,33 +19,70 @@
 //       </header>
 
 //       <section className={styles.container}>
-//         {properties.map((property, idx) => (
-//           <div key={idx} className={styles["food-section"]}>
-//             <div className={styles.right}>
-//               <div className={styles.food}>
-//                 <h3>{property.type}</h3>
+//         {properties.map((property, idx) => {
+//           let content;
+
+//           if (flip) {
+//             content = (
+//               <div key={idx} className={styles["food-section"]}>
+//                 <div className={styles.right}>
+//                   <div className={styles.food}>
+//                     <h3>{property.type}</h3>
+//                   </div>
+//                   <div className={styles.price}>
+//                     {[property.food1, property.food2, property.food3].map(
+//                       (food, i) =>
+//                         food.name && (
+//                           <h4 key={i}>
+//                             {food.name} ----{" "}
+//                             <span className={styles.Fprice}>{food.price}</span>
+//                             <br />
+//                             <span className={styles.nameEN}>{food.nameEN}</span>
+//                           </h4>
+//                         )
+//                     )}
+//                   </div>
+//                 </div>
+//                 <div className={styles.left}>
+//                   <div className={styles.pic}>
+//                     <img src={property.image} alt={property.type} />
+//                   </div>
+//                 </div>
 //               </div>
-//               <div className={styles.price}>
-//                 {[property.food1, property.food2, property.food3].map(
-//                   (food, i) =>
-//                     food.name && (
-//                       <h4 key={i}>
-//                         {food.name} ----{" "}
-//                         <span className={styles.Fprice}>{food.price}</span>{" "}
-//                         <br />
-//                         <span className={styles.nameEN}>{food.nameEN}</span>
-//                       </h4>
-//                     )
-//                 )}
+//             );
+//           } else {
+//             content = (
+//               <div key={idx} className={styles["food-section"]}>
+//                 <div className={styles.left}>
+//                   <div className={styles.pic}>
+//                     <img src={property.image} alt={property.type} />
+//                   </div>
+//                 </div>
+//                 <div className={styles.right}>
+//                   <div className={styles.food}>
+//                     <h3>{property.type}</h3>
+//                   </div>
+//                   <div className={styles.price}>
+//                     {[property.food1, property.food2, property.food3].map(
+//                       (food, i) =>
+//                         food.name && (
+//                           <h4 key={i}>
+//                             {food.name} ----{" "}
+//                             <span className={styles.Fprice}>{food.price}</span>
+//                             <br />
+//                             <span className={styles.nameEN}>{food.nameEN}</span>
+//                           </h4>
+//                         )
+//                     )}
+//                   </div>
+//                 </div>
 //               </div>
-//             </div>
-//             <div className={styles.left}>
-//               <div className={styles.pic}>
-//                 <img src={property.image} alt={property.type} />
-//               </div>
-//             </div>
-//           </div>
-//         ))}
+//             );
+//           }
+
+//           flip = !flip; 
+//           return content;
+//         })}
 //       </section>
 //     </div>
 //   );
@@ -52,7 +91,7 @@
 // export default MenuCard;
 
 
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./Menu.module.css";
 
 function MenuCard({ data }) {
@@ -74,68 +113,53 @@ function MenuCard({ data }) {
 
       <section className={styles.container}>
         {properties.map((property, idx) => {
-          let content;
-
-          if (flip) {
-            content = (
-              <div key={idx} className={styles["food-section"]}>
-                <div className={styles.right}>
-                  <div className={styles.food}>
-                    <h3>{property.type}</h3>
-                  </div>
-                  <div className={styles.price}>
-                    {[property.food1, property.food2, property.food3].map(
-                      (food, i) =>
-                        food.name && (
-                          <h4 key={i}>
-                            {food.name} ----{" "}
-                            <span className={styles.Fprice}>{food.price}</span>
-                            <br />
-                            <span className={styles.nameEN}>{food.nameEN}</span>
-                          </h4>
-                        )
-                    )}
-                  </div>
-                </div>
-                <div className={styles.left}>
-                  <div className={styles.pic}>
-                    <img src={property.image} alt={property.type} />
-                  </div>
-                </div>
+          const leftContent = (
+            <div className={styles.left}>
+              <div className={styles.pic}>
+                <img src={property.image} alt={property.type} />
               </div>
-            );
-          } else {
-            content = (
-              <div key={idx} className={styles["food-section"]}>
-                <div className={styles.left}>
-                  <div className={styles.pic}>
-                    <img src={property.image} alt={property.type} />
-                  </div>
-                </div>
-                <div className={styles.right}>
-                  <div className={styles.food}>
-                    <h3>{property.type}</h3>
-                  </div>
-                  <div className={styles.price}>
-                    {[property.food1, property.food2, property.food3].map(
-                      (food, i) =>
-                        food.name && (
-                          <h4 key={i}>
-                            {food.name} ----{" "}
-                            <span className={styles.Fprice}>{food.price}</span>
-                            <br />
-                            <span className={styles.nameEN}>{food.nameEN}</span>
-                          </h4>
-                        )
-                    )}
-                  </div>
-                </div>
-              </div>
-            );
-          }
+            </div>
+          );
 
-          flip = !flip; // toggle after each section
-          return content;
+          const rightContent = (
+            <div className={styles.right}>
+              <div className={styles.food}>
+                <h3>{property.type}</h3>
+              </div>
+              <div className={styles.price}>
+                {[property.food1, property.food2, property.food3].map(
+                  (food, i) =>
+                    food.name && (
+                      <h4 key={i}>
+                        {food.name} ----{" "}
+                        <span className={styles.Fprice}>{food.price}</span>
+                        <br />
+                        <span className={styles.nameEN}>{food.nameEN}</span>
+                      </h4>
+                    )
+                )}
+              </div>
+            </div>
+          );
+
+          const section = (
+            <div key={idx} className={styles["food-section"]}>
+              {flip ? (
+                <>
+                  {rightContent}
+                  {leftContent}
+                </>
+              ) : (
+                <>
+                  {leftContent}
+                  {rightContent}
+                </>
+              )}
+            </div>
+          );
+
+          flip = !flip;
+          return section;
         })}
       </section>
     </div>
